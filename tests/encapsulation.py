4812b10852542d76ae86195d09f8ea7af9e3bdb8
irina <neeiraaaa@gmail.com>
2022-12-19 18:23:55 +0300

    performed test encapsulation

--- tests/encapsulation.py
+++ tests/encapsulation.py
@@ -1,31 +1,61 @@
-from selene.support.shared import browser
-from selene import have
 import os
 
-def test_form():
-    browser.open('/automation-practice-form')
+from selene import command, have
+from selene.support.shared import browser
+
+
+def add_name():
     browser.element('#firstName').type('Irina')
     browser.element('#lastName').type('Rogova')
+
+
+def add_email():
     browser.element('#userEmail').type('neeiraaaa@gmail.com')
+
+
+def add_gender():
     browser.element('#gender-radio-2').double_click()
-    browser.element('#userNumber').type('79253999999')
+
+
+def date_of_birth():
     browser.element('#dateOfBirthInput').click()
     browser.element('[class="react-datepicker__month-select"]').click()
     browser.element('[value="9"]').click()
     browser.element('[class="react-datepicker__year-select"]').click()
     browser.element('[value="1997"]').click()
     browser.element('[class="react-datepicker__day react-datepicker__day--025 react-datepicker__day--weekend"]').click()
-    browser.element('#subjectsInput').set_value('eng').press_enter()
+
+
+def add_hobbies():
+    browser.element('[for="hobbies-checkbox-3"]').perform(command.js.scroll_into_view)
     browser.element('[for="hobbies-checkbox-3"]').click()
-    browser.element('#uploadPicture').set_value(
-        os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'tests/img.png')))
-    browser.element('#currentAddress').type('Москва')
+
+
+def add_subjects():
+    browser.element('#subjectsInput').set_value('eng').press_enter()
+
+
+def add_state_and_city():
     browser.element('#state').click()
     browser.element('#react-select-3-input').press_enter()
     browser.element('#city').click()
     browser.element('#react-select-4-input').press_enter()
+
+
+def element_type(selector, data):
+    browser.element(selector).type(data)
+
+
+def upload_file():
+    browser.element('#uploadPicture').set_value(
+        os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'tests/img.jpg')))
+
+
+def submit_final_button():
     browser.element('#submit').with_(click_by_js=True).click()
 
+
+def data_validation():
     browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
     browser.all('.table-responsive td:nth-child(2)').should(have.texts(
         'Irina Rogova',
@@ -35,6 +65,6 @@ def test_form():
         '25 October,1997',
         'English',
         'Music',
-        'img.png',
+        'img.jpg',
         'Москва',
         'Haryana Karnal'))
