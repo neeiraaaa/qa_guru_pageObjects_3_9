672907250e475abc3dcb1dffef01f1908f4bd6bd
irina <neeiraaaa@gmail.com>
2023-01-19 15:46:48 +0300

    first commit

--- demoqa_tests/utils/path.py
+++
@@ -1,9 +0,0 @@
-from selene.support.shared import browser
-import os
-import tests
-
-
-def create_path(element, file):
-    browser.element(element).set_value(
-        os.path.abspath(os.path.join(os.path.dirname(tests.__file__), file))
-    )
